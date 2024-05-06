641aed61b90ef11ba8fec517b5213c3434692d16
Roushankr1 <168983719+Roushankr1@users.noreply.github.com>
2024-05-06 16:38:17 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber );
+
+// console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+
+let isloggedIn = "Roushan"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(typeof booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Roushan" => true
+
+let somenumber = 23445
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
