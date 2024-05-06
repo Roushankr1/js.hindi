84cf6404605b94755f63f03b45815240e9910089
Roushankr1 <168983719+Roushankr1@users.noreply.github.com>
2024-05-06 04:22:58 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = "144553"
+let accountEmail = "roushan@gmail.com"
+var accountpassword = "123456"
+accountcity = "patna"
+let accountstate;
+
+// accountId = 456 // not to prefer
+
+accountEmail = "roushan1325@gmail.com"
+accountpassword = "1223122"
+accountcity = "Ara"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountpassword, accountcity, accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 + 3) // we are usimg nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Roushan")
+
+let Name = "Roushan"
+let age = 18
+let isloggedin = true
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value 
+// undefined =>
+// symbol =>
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
