e8d3e9044fe180a94a3cb094018ace7cd53175c3
Roushankr1 <168983719+Roushankr1@users.noreply.github.com>
2024-05-08 08:52:31 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive 
+
+// 7 types string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+
+// console.log(id === anotherId);
+
+const bigNumber = 98564165101651603216516n
+
+
+// Refrence (non-primitive)
+
+// array, object, functions
+
+const heros = ["shaktiman", "naagaraj", "doga"]
+
+let myobj = {
+    name: "Roushan",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("hrllo world");
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
