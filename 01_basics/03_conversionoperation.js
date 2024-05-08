06b4ad88caa1dd2369a09893edadf2f5de0b07c9
Roushankr1 <168983719+Roushankr1@users.noreply.github.com>
2024-05-08 07:13:26 +0000

    operation str or num

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -27,6 +27,44 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 let somenumber = 23445
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************** operations ********************
+
+let value = 33
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Roushan"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2 );
+// console.log( 1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log( 1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = (2 + 2)
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
 
